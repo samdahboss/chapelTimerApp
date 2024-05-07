37b0083b6df01ce2f71dd1935adf485245541c8a
samdahboss <131814523+samdahboss@users.noreply.github.com>
2024-05-07 21:18:54 +0100

    final commit

--- js/output.js
+++ js/output.js
@@ -3,7 +3,12 @@ let presenterCountDownDisplay=document.getElementById('presenterCountdownDisplay
 
 let timer=setInterval(function(){
     presenterSessionDisplay.innerText=localStorage.getItem('sessionNameContent')
+    presenterCountDownDisplay.style.color="black"
+    if (presenterCountDownDisplay.innerText=="TIME IS UP"){
+        presenterCountDownDisplay.style.color="red"
+    }
     presenterCountDownDisplay.innerText=localStorage.getItem('countDownDisplayContent')
+    
 },1000)
 
 
